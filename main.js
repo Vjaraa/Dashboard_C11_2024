d456ffb32b1b3becd4b26b79358e7785e7e89204
Valeria Jara <valeria.jaraarenas@gmail.com>
2024-02-25 12:02:44 -0300

    Adding the final part of the indicators

--- main.js
+++ main.js
@@ -273,4 +273,69 @@ fetch(apiUrlDetailPorsche)
   });
 
 
+
+    /* Fifth Car detail API call
+*/
+  
+const apiUrlDetailTrailblazer = 'https://parallelum.com.br/fipe/api/v1/carros/marcas/23/modelos/6268/anos/2019-3';
+
+fetch(apiUrlDetailTrailblazer)
+  .then(response => {
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+
+    const modelFifthCar = document.getElementById('detail_fifth_car');
+    const priceFifthCar = document.getElementById('price_fifth_car');
+    const yearFifthCar = document.getElementById('year_fifth_car');
+    const brandFifthCar = document.getElementById('brand_fifth_car');
+
+    var json = JSON.parse(JSON.stringify(data));
+
+    modelFifthCar.textContent = json.Modelo;
+    priceFifthCar.textContent = json.Valor;
+    yearFifthCar.textContent = json.AnoModelo;
+    brandFifthCar.textContent = json.Marca;
+
+  })
+  .catch(error => {
+    console.error('Hubo un problema con la petición fetch:', error);
+  });
+
+
+    /* Sixth Car detail API call
+*/
+  
+const apiUrlDetailElantra = 'https://parallelum.com.br/fipe/api/v1/carros/marcas/26/modelos/7757/anos/2018-1';
+
+fetch(apiUrlDetailElantra)
+  .then(response => {
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+
+    const modelSixthCar = document.getElementById('detail_sixth_car');
+    const priceSixthCar = document.getElementById('price_sixth_car');
+    const yearSixthCar = document.getElementById('year_sixth_car');
+    const brandSixthCar = document.getElementById('brand_sixth_car');
+
+    var json = JSON.parse(JSON.stringify(data));
+
+    modelSixthCar.textContent = json.Modelo;
+    priceSixthCar.textContent = json.Valor;
+    yearSixthCar.textContent = json.AnoModelo;
+    brandSixthCar.textContent = json.Marca;
+
+  })
+  .catch(error => {
+    console.error('Hubo un problema con la petición fetch:', error);
+  });
   
